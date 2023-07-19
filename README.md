9dac033e80e1284ddfaf078df5cf6d33c560c752
Lucas Saymon <102837549+lucassaymon1@users.noreply.github.com>
2023-07-19 13:41:22 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Clone-tabnews
+
 Implementação do https://tabnews.com.br para o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,43 @@
-import { useState } from "react"
+import { useState } from "react";
 
-function Home(){
-    const [isLove, setIsLove] = useState(false)
+function Home() {
+  const [isLove, setIsLove] = useState(false);
 
-    function handleMoveButton(){
-        const noButton = document.querySelector("#no-btn")
-        noButton.style.position = "absolute"
+  function handleMoveButton() {
+    const noButton = document.querySelector("#no-btn");
+    noButton.style.position = "absolute";
 
-        const screenWidth = screen.width/1.2
-        const screenHeight = screen.height/1.2
+    const screenWidth = screen.width / 1.2;
+    const screenHeight = screen.height / 1.2;
 
-        const x = Math.floor(Math.random()*screenWidth) + 1
-        const y = Math.floor(Math.random()*screenHeight) + 1
+    const x = Math.floor(Math.random() * screenWidth) + 1;
+    const y = Math.floor(Math.random() * screenHeight) + 1;
 
-        noButton.style.left = x+"px"
-        noButton.style.top = y+"px"
-    }
-    return(
+    noButton.style.left = x + "px";
+    noButton.style.top = y + "px";
+  }
+  return (
+    <div>
+      <h1>
+        {isLove
+          ? "Uhuuuu! (ﾉ´ з `)ノ"
+          : "Olá irmãzinha, eu amo você, você também me ama??? 🥺"}
+      </h1>
+
+      {!isLove && (
         <div>
-            <h1>{
-                    isLove ? "Uhuuuu! (ﾉ´ з `)ノ" : "Olá irmãzinha, eu amo você, você também me ama??? 🥺"}</h1>
-
-
-            {!isLove && 
-                <div>
-                    <button onClick={() => setIsLove(true)}>Sim! 🥰</button>
-                    <button id="no-btn" onMouseOver={handleMoveButton} onClick={handleMoveButton}>Não 😒</button>
-
-                </div>
-            }
-            
-
+          <button onClick={() => setIsLove(true)}>Sim! 🥰</button>
+          <button
+            id="no-btn"
+            onMouseOver={handleMoveButton}
+            onClick={handleMoveButton}
+          >
+            Não 😒
+          </button>
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
