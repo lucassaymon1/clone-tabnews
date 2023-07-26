eb6517fe07a7bc8ed25ba484525efb2c9d12729d
Lucas Saymon <102837549+lucassaymon1@users.noreply.github.com>
2023-07-26 13:54:37 +0000

    adiciona página com idéias sobre a plataforma `tabhub.com.br`

---
+++ pages/ideas.js
@@ -0,0 +1,30 @@
+function Ideas() {
+  return (
+    <div>
+      <h1>Página criada para abrigar ideias sobre o conteúdo do TabHub</h1>
+      <main>
+        <p>
+          {"-->"} Um local para se discutir sobre jogos, especialmente
+          lançamentos e que possuam uma campanha.
+        </p>
+        <p>
+          {"-->"} Usuários poderão discutir sobre fases, chefes, técnicas de
+          jogabilidade, puzzles, detalhes e segredos sobre a história dos jogos
+          que estão jogando.
+        </p>
+        <p>
+          {"-->"} Tudo isso será feito com o propósito de espalhar conhecimento
+          útil sobre jogos, unindo comunidades com um interesse mútuo, ajudando
+          jogadores novos e levantando discursões relevantes
+        </p>
+        <p>
+          {"-->"} Estará fora dos interesses da plataforma promover discursões
+          sobre questões delicadas ou provocativas, que incentivem agressividade
+          entre os usuários
+        </p>
+      </main>
+    </div>
+  );
+}
+
+export default Ideas;
