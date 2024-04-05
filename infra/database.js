42eb43bc6cad1a3849f33938ed93f447e1abfbc5
Lucas Saymon <102837549+lucassaymon1@users.noreply.github.com>
2024-04-05 23:46:09 +0000

    makes database.js client.connect() more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -9,17 +9,18 @@ async function query(objectQuery) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
-  const client = await pool.connect();
   // await client.connect();
 
   // objectQuery refers to the query directed to the database
   try {
+    const client = await pool.connect();
     const result = await client.query(objectQuery);
-    return result;
-  } catch (err) {
-    console.error(err);
-  } finally {
     client.release();
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
     await pool.end();
   }
 }
